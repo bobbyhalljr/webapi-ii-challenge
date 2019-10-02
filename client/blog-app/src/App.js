839c54f91f6fcd0816ae7bd568fd5d59cbe007b4
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-10-02 11:44:10 -0400

    added front-end /client and cors to project

--- client/blog-app/src/App.js
+++ client/blog-app/src/App.js
@@ -4,22 +4,29 @@ import axios from 'axios';
 import './App.css';
 
 import { PostsContext } from './contexts/PostsContext';
+import Posts from './components/Posts';
 
 const App = () => {
   const [posts, setPosts] = useState([])
 
-  useEffect(() => {
+  const getPosts = () => {
     axios.get('http://localhost:4000/api/posts')
     .then(res => {
       console.log(res.data)
+      setPosts(res.data)
     })
     .catch(err => console.log(err.response))
+  }
+
+  useEffect(() => {
+    getPosts();
   }, [])
 
   return (
-    <PostsContext.Provider value={posts}>
+    <PostsContext.Provider state={{ posts, setPosts, getPosts }}>
       <div className="App">
-        <h1>Hello from app.js</h1>
+        <h1>Welcome to Bobby's Blog</h1>
+        <Posts posts={posts} setPosts={setPosts} />
       </div>
     </PostsContext.Provider>
   );
